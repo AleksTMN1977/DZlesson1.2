315077f5624f1dfc1f9f27472b1da372bdcc6a5f
AleksTMN <kirilloff_77@mail.ru>
2022-03-04 12:30:29 +0500

    4commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,18 +7,18 @@ public class Main {
         byte a = 12;
         short b = 12356;
         int c = - 528;
-        long d = 56l;
+        long d = 56L;
         float e = 36.6f;
         double g = 3.14;
         char h = 15;
-        boolean j = 10 > 5;
+        boolean j = true;
 
         System.out.println("Задача 2");
         var weight1boxer = 78.2;
         var weight2boxer = 82.7;
         double totalWeight = weight1boxer + weight2boxer;
         System.out.println("Общий вес бойцов " + totalWeight + " кг");
-        double weightDifference = (weight1boxer - weight2boxer) % weight2boxer;
+        double weightDifference = weight2boxer % weight1boxer;
         System.out.println("Разница в весе боксеров " + weightDifference + " кг");
 
         System.out.println("Задача 3");
